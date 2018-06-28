64b02156b47b589717933157887d5c846e42bda2
cildhdi <1961464399@qq.com>
2018-06-28 13:06:02 +0800

    ...

--- function_parsing.h
+++ function_parsing.h
@@ -5,13 +5,30 @@
 #include <memory>
 #include <algorithm>
 #include <vector>
+#include <iostream>
 
 namespace fp
 {
 	class base_function;
 	using base_function_ptr = std::shared_ptr<base_function>;
-
 	base_function_ptr function_parse(std::string func_str);
+	std::ostream& operator<<(std::ostream& os, base_function_ptr& func);
+
+
+	enum FunctionType
+	{
+		T_BASE_FUNCTION,
+		T_INDEPENDENT_VARIABLE,
+		T_CONSTANT_FUNCTION,
+		T_ADD_FUNCTION,
+		T_MINUS_FUNCTION,
+		T_MULTIPLY_FUNCTION,
+		T_DIVIDE_FUNCTION,
+		T_POWER_FUNCTION,
+		T_LN_FUNCTION,
+		T_SIN_FUNCTION,
+		T_COS_FUNCTION
+	};
 
 	class base_function
 	{
@@ -19,6 +36,8 @@ namespace fp
 		virtual std::string str() = 0;
 		virtual double value(double x) = 0;
 		virtual base_function_ptr derivative() = 0;
+		const FunctionType _type;
+		base_function(FunctionType type) :_type(type) {}
 	};
 
 	class independent_variable :public base_function
